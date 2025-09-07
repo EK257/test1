12bc043d81326f174a1ccb2e0bbb99e7e607b954
EK257 <klime777@ya.ru>
2025-09-07 13:56:50 +0300

    MultiplyTest.java

--- test1/src/test/java/MultiplyTest.java
+++ test1/src/test/java/MultiplyTest.java
@@ -8,40 +8,41 @@ public class MultiplyTest {
     @Test
     public void bigIntMultiply() {
         Multiply multiply = new Multiply();
-        assertEquals("15", multiply.bigIntMultiply("3", "5"));
-        assertEquals("-15", multiply.bigIntMultiply("-3", "5"));
-        assertEquals("-15", multiply.bigIntMultiply("3", "-5"));
-        assertEquals("15", multiply.bigIntMultiply("-3", "-5"));
+        assertEquals("24", multiply.bigIntMultiply("6", "4"));
+        assertEquals("-24", multiply.bigIntMultiply("-6", "4"));
+        assertEquals("-24", multiply.bigIntMultiply("6", "-4"));
+        assertEquals("24", multiply.bigIntMultiply("-6", "-4"));
         assertEquals("0", multiply.bigIntMultiply("0", "1"));
     }
 
     @Test
     public void secondMultiply() {
         Multiply multiply = new Multiply();
-        assertEquals(15, multiply.secondMultiply(3, 5));
-        assertEquals(-15, multiply.secondMultiply(-3, 5));
-        assertEquals(-15, multiply.secondMultiply(3, -5));
-        assertEquals(15, multiply.secondMultiply(-3, -5));
+        assertEquals(24, multiply.secondMultiply(6, 4));
+        assertEquals(-24, multiply.secondMultiply(-6, 4));
+        assertEquals(-24, multiply.secondMultiply(6, -4));
+        assertEquals(24, multiply.secondMultiply(-6, -4));
         assertEquals(0, multiply.secondMultiply(0, 1));
     }
 
     @Test
     public void simpleMultiply() {
         Multiply multiply = new Multiply();
-        assertEquals(15, multiply.simpleMultiply(3, 5));
-        assertEquals(-15, multiply.simpleMultiply(-3, 5));
-        assertEquals(-15, multiply.simpleMultiply(3, -5));
-        assertEquals(15, multiply.simpleMultiply(-3, -5));
+        assertEquals(24, multiply.simpleMultiply(6, 4));
+        assertEquals(-24, multiply.simpleMultiply(-6, 4));
+        assertEquals(-24, multiply.simpleMultiply(6, -4));
+        assertEquals(24, multiply.simpleMultiply(-6, -4));
         assertEquals(0, multiply.simpleMultiply(0, 1));
     }
 
     @Test
     public void fastMultiply() {
         Multiply multiply = new Multiply();
-        assertEquals(15, multiply.fastMultiply(3, 5));
-        assertEquals(-15, multiply.fastMultiply(-3, 5));
-        assertEquals(-15, multiply.fastMultiply(3, -5));
-        assertEquals(15, multiply.fastMultiply(-3, -5));
+        assertEquals(24, multiply.fastMultiply(6, 4));
+        assertEquals(-24, multiply.fastMultiply(-6, 4));
+        assertEquals(-24, multiply.fastMultiply(6, -4));
+        assertEquals(24, multiply.fastMultiply(-6, -4));
         assertEquals(0, multiply.fastMultiply(0, 1));
     }
 }
+
