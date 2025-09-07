cb8fa32fc49cb16f99a689d88bd56c0ce0cbda6a
EK257 <klime777@ya.ru>
2025-09-07 13:51:13 +0300

    Program.java

--- test1/src/main/java/Program.java
+++ test1/src/main/java/Program.java
@@ -1,13 +1,14 @@
 public class Program {
     public static void main(String[] args){
         Multiply multiply = new Multiply();
-        long mul = multiply.secondMultiply(3,5);
+        long mul = multiply.secondMultiply(6,4);
         System.out.println(mul);
-        mul = multiply.simpleMultiply(3,5);
+        mul = multiply.simpleMultiply(6,4);
         System.out.println(mul);
-        mul = multiply.fastMultiply(3,5);
+        mul = multiply.fastMultiply(6,4);
         System.out.println(mul);
-        String mul1 = multiply.bigIntMultiply("3","5");
+        String mul1 = multiply.bigIntMultiply("6","4");
         System.out.println(mul1);
     }
 }
+
